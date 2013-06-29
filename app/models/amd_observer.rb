259fcbb8abb7bfab1153381f59453c7356c52ab8
Néstor Coppi <nestorcoppi@gmail.com>
2013-06-29 15:07:57 -0300

    Updates incrementales listos =D

--- app/models/amd_observer.rb
+++ app/models/amd_observer.rb
@@ -4,7 +4,7 @@ class AmdObserver < ActiveRecord::Observer
 
   # observe only #create actions here
   def after_create(record)
-    table = record.class.table_name
+    table = table_name(record)
 
     keys = values = ''
     i = 0
@@ -15,19 +15,49 @@ class AmdObserver < ActiveRecord::Observer
         values += ', '
       end
 
+      p key
+      p value
+
       keys += key
-      values += if value.is_a?(Numeric)
-                 value.to_s
-               else
-                 "'#{value.to_s}'"
-               end
-      i += 1
+      values += if value.is_a?(Numeric) 
+                  value.to_s
+                else 
+                  "'#{value.to_s.gsub("'",'\"')}'"
+                end
+      i = 1
     end
 
     query = "INSERT INTO #{table} (#{keys}) VALUES (#{values});"
 
     %x{echo "#{query}" >> #{Rails.root}/cont-bk}
-    
     true
+  end
+
+  def after_update(record)
+    attrs = {}
+    set = ''
+    i = 0
+
+    record.changes.each do |key, values| 
+      new = values.last
+      set += ', ' unless i.zero?
+      set += " \"#{key}\" = "
+      set += (new.is_a?(Numeric) ? new.to_s : "'#{new.to_s}'")
+      i = 1
+    end
+
+    table = table_name(record)
+    identifier = "#{table}.id = #{record.id}"
+
+    query = "UPDATE \"#{table}\" SET #{set} WHERE #{identifier};"
+
+    %x{echo "#{query}" >> #{Rails.root}/cont-bk}
+    true
+  end
+
+  private
+
+  def table_name(record)
+    record.class.table_name
   end
 end
