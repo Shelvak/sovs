c3d9e36d6ecce321503dec1dc7271d29dcf2dbb1
Néstor Coppi <nestorcoppi@gmail.com>
2017-04-23 19:48:12 -0300

    Deploying

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.version = '1.0'
-
 Rails.application.config.assets.precompile += %w( print.css graphs.js )
+Rails.application.config.assets.initialize_on_precompile = false
+
